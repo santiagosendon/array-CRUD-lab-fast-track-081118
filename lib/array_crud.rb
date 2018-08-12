472106891f23613641d7b6fdb9c47bcca7d04ed5
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-12 03:42:27 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   remove_element_from_end_of_array = [element]
-  remove_element_from_end_of_array.pop = elem
+  remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
