3cf9521b9fac2199b3d3ed6da6991fcb885e7d39
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-12 03:40:40 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,24 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  types_of_dogs = ["pitbull","shitzu","moltese","poodle"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   add_element_to_end_of_array = [array]
+   add_element_to_end_of_array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array = [array]
+  add_element_to_start_of_array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_element_from_end_of_array = ["array","element"]
+  "element" = remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
